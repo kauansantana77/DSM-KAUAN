864ebc9cac91abffebe2ac879cf47b4c3358a496
kauansantana77 <103324687+kauansantana77@users.noreply.github.com>
2022-04-20 16:15:13 -0300

    questao2

--- App.js
+++ App.js
@@ -1,48 +1,24 @@
-import React, { Component } from 'react'
-import {StyleSheet,TouchableOpacity, Text, View,} from 'react-native'
+import React from "react";
+import { View, Text } from "react-native";
 
-class App extends Component {
-  state = {
-    count: 0
-  }
+const quadradosColoridos = () => {
+  return (
+    <View
+      style={{
+        flexDirection: "column",
+        height: 500,
+        padding: 20,
+        justifyContent:'space-between'
+      }}
+    >
+      <View style={{ backgroundColor: "blue", flex: 0.3 }} />
+      <Text>Square 1</Text>
+      <View style={{ backgroundColor: "red", flex: 0.3 }} />
+      <Text>Square 2</Text>
+      <View style={{ backgroundColor: "yellow", flex: 0.3 }} />
+      <Text>Square 3</Text>
+    </View>
+  );
+};
 
-  onPress = () => {
-    this.setState({
-      count: this.state.count + 1
-    })
-  }
-
- render() {
-    return (
-      <View style={styles.container}>
-        <TouchableOpacity
-         style={styles.button}
-         onPress={this.onPress}
-        >
-         <Text>APERTE</Text>
-        </TouchableOpacity>
-        <View>
-          <Text>
-            Você clicou { this.state.count } vezes!
-          </Text>
-        </View>
-      </View>
-    )
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  button: {
-    alignItems: 'center',
-    backgroundColor: '#DDDDDD',
-    padding: 10,
-    marginBottom: 10
-  }
-})
-
-export default App;
+export default quadradosColoridos;
